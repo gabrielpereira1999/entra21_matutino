bc921e8c59c0d7d4aa9d4f31940532e7fe18ca21
i3i <i3i@sala125.proway.treina>
2019-05-21 09:24:55 -0300

    trabalho

---
+++ Java/Trabalho 2/src/telaLogin/TelaLogin.java
@@ -0,0 +1,37 @@
+package telaLogin;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+
+public class TelaLogin extends JFrame {
+
+	private JPanel contentPane;
+
+	public TelaLogin() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblUsuario = new JLabel("Usuario");
+		lblUsuario.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblUsuario.setBounds(10, 21, 56, 19);
+		contentPane.add(lblUsuario);
+		
+		JLabel lblSenha = new JLabel("Senha");
+		lblSenha.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblSenha.setBounds(10, 46, 48, 19);
+		contentPane.add(lblSenha);
+		
+		
+		
+	}
+}
