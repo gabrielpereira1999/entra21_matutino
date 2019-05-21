b9adac39d94c1affc4e55354d24cc3423574e1c0
i3i <i3i@sala125.proway.treina>
2019-05-21 08:57:11 -0300

    Update added 31 and 32

---
+++ Java/31 ExportarJar/src/pacote/Texto.java
@@ -0,0 +1,18 @@
+package pacote;
+
+public class Texto {
+	
+	//Atributo
+	private String palavra;
+	
+	//Construtor
+	public Texto(String palavra) {
+		this.palavra = palavra;
+	}
+	
+	//Método para contar caracteres
+	public void contarLetras() {
+		System.out.println("A palavra "+palavra.toLowerCase()+" possui "+palavra.length()+" letras.");
+	}
+
+}
---
+++ Java/32 ImportarJar/src/Principal.java
@@ -0,0 +1,12 @@
+import pacote.Texto;
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+		Texto t = new Texto("Proway");
+		t.contarLetras();
+
+	}
+
+}
